c2f296a290f842d1ec6c31be04c0e405e349d033
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-03 15:15:03 +0200

    cleanup

--- app/types/blocks.ts
+++ app/types/blocks.ts
@@ -1,6 +1,6 @@
 import type { Page } from 'payload/generated-types';
 
-export type Block = Page['layout'][0];
+export type Block = Required<Page>['layout'][0];
 
 export type PostsListBlock = Extract<Block, { blockType: 'postsList' }>;
 
@@ -9,7 +9,3 @@ export function isPostsList(item: Block): item is PostsListBlock {
 }
 
 export type ScreeningsListBlock = Extract<Block, { blockType: 'screeningsList' }>;
-
-export function isScreeningsList(item: Block): item is ScreeningsListBlock {
-  return item.blockType === 'screeningsList';
-}
