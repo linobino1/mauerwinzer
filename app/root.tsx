f527bc32cac407f9d3a09b278627625b692df148
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-17 12:31:21 +0200

    captcha validation script moved to root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -93,6 +93,31 @@ export function useChangeLanguage(locale: string) {
   }, [locale, i18n]);
 }
 
+const validateCaptcha = async (token: string): Promise<boolean> => {
+  if (environment().NODE_ENV === 'development') {
+    console.log('Captcha validation skipped in development mode');
+    return true;
+  }
+  fetch('https://hcaptcha.com/siteverify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      secret: environment().HCAPTCHA_SECRET_KEY,
+      sitekey: environment().HCAPTCHA_SITE_KEY,
+      response: token,
+    }),
+  })
+  .then((res) => res.json())
+  .then((json) => {
+    if (json.success) {
+      return true;
+    }
+  });
+  return false;
+}
+
 /**
  * form handler for modal forms (reservation)
  */
