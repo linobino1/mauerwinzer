4b8b4fecdc04ed78b6cf5c049381df8fe79dd02d
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-17 14:14:25 +0200

    make mobile menu a standard modal

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useEffect } from 'react';
+import React from 'react';
 import type { Media } from "payload/generated-types";
 import { Navigation } from '../Navigation';
-import { Link, useNavigate } from '@remix-run/react';
+import { Link, useNavigate, useSearchParams } from '@remix-run/react';
 import { Image } from '~/components/Image';
 import type { Site, Navigation as NavigationType } from 'payload/generated-types';
 import classes from './index.module.css';
 import Button from '../Button';
+import Modal from '../Modal';
 
 type Props = {
   site: Site
@@ -17,48 +18,17 @@ type Props = {
 const Header: React.FC<Props> = ({
   site, navigations, content,
 }) => {
-  const [menuVisible, setMenuVisible] = React.useState<boolean>();
-  const menuOpen = () => {
-    setMenuVisible(true);
-  };
-
-  const menuClose = () => {
-    setMenuVisible(false);
-  };
-  
   const navigate = useNavigate();
+  const [ searchParams ] = useSearchParams();
   
-  useEffect(() => {
-    if (menuVisible) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
-    }
-    
-  }, [menuVisible]);
-
   return (
     <header>
       <div className={classes.mainHeader}>
-        <div className={`${classes.navMainContainer} ${menuVisible && classes.visible}`}>
-          <div className={classes.menuHeader}>
-            <button
-              onClick={menuClose}
-              type="button"
-              className={classes.menuClose}
-            />
-          </div>
+        <div className={`${classes.navMainContainer}`}>
           <div className={classes.menuBody}>
             <Navigation
               navigation={navigations.find((x) => x.type === 'main')}
               className={classes.navMain}
-            />
-            <Button
-              className={classes.reservationButton}
-              layout="big"
-              label={site.headerButton.label}
-              onClick={() => navigate('?modal=reservation', { preventScrollReset: true })}
-              data-mobile-only="true"
             />
           </div>
         </div>
@@ -72,19 +42,36 @@ const Header: React.FC<Props> = ({
             />
           )}
         </Link>
-        <button
-          onClick={menuOpen}
-          type="button"
-          className={classes.menuOpen}
+        <Button
+          layout="symbol"
+          symbol="menu"
+          className={classes.menuButton}
+          onClick={() => navigate('?modal=menu', { preventScrollReset: true })}
         />
         <Button
-          className={classes.reservationButton}
           layout="big"
           label={site.headerButton.label}
+          className={classes.reservationButton}
           onClick={() => navigate('?modal=reservation', { preventScrollReset: true })}
         />
       </div>
       {content}
+      { searchParams.get('modal') === 'menu' && (
+        <Modal>
+          <div className={classes.mobileMenu}>
+            <Navigation
+              navigation={navigations.find((x) => x.type === 'main')}
+              className={classes.navMobile}
+            />
+            <Button
+              className={classes.reservationButton}
+              layout="big"
+              label={site.headerButton.label}
+              onClick={() => navigate('?modal=reservation', { preventScrollReset: true })}
+            />
+          </div>
+        </Modal>
+      )}
     </header>
   );
 };
