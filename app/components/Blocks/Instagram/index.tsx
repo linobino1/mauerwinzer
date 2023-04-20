88c007007fd4501dbe2a185124d31a9a7dcda5fa
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-20 13:14:35 +0200

    instagram embed: fix loading external script

--- app/components/Blocks/Instagram/index.tsx
+++ app/components/Blocks/Instagram/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './index.module.css';
-
 
 export type Type = {
   blockType: 'instagram'
@@ -9,10 +8,21 @@ export type Type = {
 }
 
 export const Instagram: React.FC<Type> = ({ beholdId }) => {
+  
+  const [, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const scriptTag = document.createElement('script');
+    scriptTag.type = 'module';
+    scriptTag.src = 'https://w.behold.so/widget.js';
+    scriptTag.addEventListener('load', () => setLoaded(true));
+    document.body.appendChild(scriptTag);
+  }, []);
+              
   return (
     <div className={classes.container}>
       <figure data-behold-id={beholdId}></figure>
-      <script src="https://w.behold.so/widget.js" type="module"></script>
+      {/* <script src="https://w.behold.so/widget.js" type="module"></script> */}
     </div>
   )
 };
