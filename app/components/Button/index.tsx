3c4b967229eb0371fbf098920b6f4874729de6f4
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-20 09:07:45 +0200

    aria-label for symbol buttons

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import classes from "./index.module.css"
+import { useTranslation } from 'react-i18next';
 
 export interface Props extends React.HTMLAttributes<HTMLButtonElement> {
   layout?: 'big' | 'submit' | 'cancel' | 'symbol' | undefined
@@ -12,9 +13,19 @@ export interface Props extends React.HTMLAttributes<HTMLButtonElement> {
 
 // export const imageUrl = (image: Media, width: number, height: number): string => mediaUrl(`${image.sizes.card.}`)
 export const Button: React.FC<Props> = (props) => {
+  let { t } = useTranslation();
   let { layout, color, symbol } = props;
   let updatedProps = { ...props };
   updatedProps.className = `${classes.button} ${props.className}`;
+  
+  // add aria-label to button if symbol is set
+  if (symbol) {
+    const symbolLabelMap = {
+      close: t('Close'),
+      menu: t('Menu'),
+    }
+    updatedProps["aria-label"] = symbolLabelMap[symbol];
+  }
   
   return (
     <button
