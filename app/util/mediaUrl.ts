41ee615be77434074667017222f0c6b983265b86
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-25 22:30:44 +0200

    move back to absolute media urls

--- app/util/mediaUrl.ts
+++ app/util/mediaUrl.ts
@@ -1,3 +1,7 @@
+import environment from "./environment"
+
 export const mediaUrl = (filename: string): string => (
-  `/media/${filename}`
+  // relative path works too, but has some caveats, e.g. the og:image tag needs an absolute path
+  // `/media/${filename}`
+  `${environment().PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000'}/media/${filename}`
 )
