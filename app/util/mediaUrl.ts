cf8cf5a1319fc92c22c5f9a0062e8e2806c84bd4
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-04-25 09:36:29 +0200

    try relative path as mediaUrl

--- app/util/mediaUrl.ts
+++ app/util/mediaUrl.ts
@@ -1,5 +1,3 @@
-import environment from "./environment"
-
 export const mediaUrl = (filename: string): string => (
-  `${environment().PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000'}/media/${filename}`
+  `/media/${filename}`
 )
