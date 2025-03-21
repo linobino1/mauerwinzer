f5432bce8595e65eb507598a40b09cb9b14b13f8
Leo <leo.hilsheimer@gmail.com>
2025-03-21 16:58:36 +0100

    chore: use new instagram API

--- frontend/app/components/Blocks/Instagram/index.tsx
+++ frontend/app/components/Blocks/Instagram/index.tsx
@@ -1,38 +1,42 @@
-import React, { useEffect, useState } from "react";
-import classes from "./index.module.css";
-import { useCookieConsent } from "~/providers/Cookies";
-import { useTranslation } from "react-i18next";
-import Button from "~/components/Button";
+import React, { useEffect, useState } from 'react'
+import classes from './index.module.css'
+import { useCookieConsent } from '~/providers/Cookies'
+import { useTranslation } from 'react-i18next'
+import Button from '~/components/Button'
 
 export type Type = {
-  blockType: "instagram";
-  blockName?: string | null;
-  apiUrl: string;
-};
+  blockType: 'instagram'
+  blockName?: string | null
+  apiUrl: string
+}
+
+type ApiResponse = {
+  posts: any[]
+}
 
 export const Instagram: React.FC<Type> = ({ apiUrl }) => {
-  const [data, setData] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-  const { consent, resetConsent } = useCookieConsent();
-  const { t } = useTranslation();
+  const [data, setData] = useState<ApiResponse | null>(null)
+  const [loaded, setLoaded] = useState(false)
+  const { consent, resetConsent } = useCookieConsent()
+  const { t } = useTranslation()
 
   useEffect(() => {
-    (async () => {
-      setLoaded(false);
-      const data = await fetch(apiUrl).then((res) => res.json());
-      setData(data);
-      setLoaded(true);
-    })();
-  }, [apiUrl]);
+    ;(async () => {
+      setLoaded(false)
+      const data = await fetch(apiUrl).then((res) => res.json())
+      setData(data)
+      setLoaded(true)
+    })()
+  }, [apiUrl])
 
   return consent ? (
     loaded ? (
       <div className={classes.container}>
-        {data.map((item: any) => (
+        {data?.posts.map((item: any) => (
           <div
             key={item.id}
             className={classes.item}
-            onClick={() => window.open(item.permalink, "_blank")}
+            onClick={() => window.open(item.permalink, '_blank')}
           >
             <div>
               <img src={item.mediaUrl} alt={item.caption} />
@@ -48,12 +52,10 @@ export const Instagram: React.FC<Type> = ({ apiUrl }) => {
     )
   ) : (
     <div className={classes.noConsent}>
-      <p>{t("cookies.noConsent")}</p>
-      {consent !== null && (
-        <Button onClick={resetConsent}>{t("cookies.resetConsent")}</Button>
-      )}
+      <p>{t('cookies.noConsent')}</p>
+      {consent !== null && <Button onClick={resetConsent}>{t('cookies.resetConsent')}</Button>}
     </div>
-  );
-};
+  )
+}
 
-export default Instagram;
+export default Instagram
